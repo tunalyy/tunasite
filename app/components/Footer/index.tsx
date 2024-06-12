defe43fb910577e1e335d9fadd53d233428e7513
tunaxz <dilkimehmettunaa@gmail.com>
2024-06-12 22:16:48 +0300

    index.tsx

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="container mx-auto w-11/12 sm:w-9/12 justify-between flex text-[#858585] selection:text-[#ccc] pb-2 select-text">
-      <span>Copyright © 2023-2024 wondexz</span>
-      <span>Made with love by <Link href={"https://github.com/wondexz"} className="hover:underline hover:text-[#ccc] transition-all duration-200">@wondexz</Link> </span>
+      <span>Copyright © 2024-2025 tunaxz</span>
+      <span>Made with love by <Link href={"https://discord.gg/QzbWSspE"} className="hover:underline hover:text-[#ccc] transition-all duration-200">@tunaxz</Link> </span>
     </footer>
   );
 }
